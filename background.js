27ff561c459f14188bf4e6a2db15f3dc800ccfd2
Anshul Bansal <bansalanshul11@yahoo.com>
2022-03-11 10:59:39 +0530

    Update background.js
    Adding more links and optimizing the code

--- background.js
+++ background.js
@@ -1,37 +1,42 @@
 // // only allowed url will be added to history
 
-allowedUrls = [
+let allowedUrls = [
   {
-    link: "www.twitter.com",
+    link: "twitter.com",
   },
   {
-    link: "www.facebook.com",
+    link: "facebook.com",
   },
   {
-    link: "www.instagram.com",
+    link: "instagram.com",
   },
   {
-    link: "www.linkedin.com",
+    link: "linkedin.com",
   },
   {
-    link: "https://web.whatsapp.com/",
+    link: "web.whatsapp.com",
   },
   {
-    link: "https://www.reddit.com/",
+    link: "reddit.com",
   },
   {
-    link: "https://in.pinterest.com/",
+    link: "pinterest.com",
   },
+  {
+    link: "primevideo.com",
+  },
+  {
+    link: "netflix.com",
+  }
 ];
 
-this.allowedUrls.forEach((element) => {
-  if (element.link.includes(window.location.hostname)) {
-    document.body.innerHTML = "";
-    let createImg = document.createElement("img");
-    createImg.style.margin = "0 auto";
-    createImg.style.height = "100vh";
-    createImg.style.display = "flex";
-    createImg.src = "https://i.ibb.co/r0RScrj/banner.jpg";
-    document.body.appendChild(createImg);
+allowedUrls.forEach((element) => {
+  if(element.link.includes(window.location.origin)) {
+    let img = document.createElement("img");
+    img.style.margin = "0 auto";
+    img.style.height = "100vh";
+    img.style.display = "flex";
+    img.src = "https://i.ibb.co/r0RScrj/banner.jpg";
+    document.getElementsByTagName('body')[0].innerHTML = img;
   }
 });
