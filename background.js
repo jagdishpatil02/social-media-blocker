4c0bf39588b1955540c78c116509d90be5458366
Jagdish Patil <patil.jagdish222@gmail.com>
2022-03-11 15:37:13 +0530

    remove netflix and prime url for now

--- background.js
+++ background.js
@@ -21,16 +21,11 @@ let blockedUrls = [
   {
     link: "pinterest.com",
   },
-  {
-    link: "primevideo.com",
-  },
-  {
-    link: "netflix.com",
-  }
 ];
 
 blockedUrls.forEach((element) => {
-  if(window.location.origin.includes(element.link)) {
-    document.getElementsByTagName("body")[0].innerHTML = '<div><img src="https://i.ibb.co/r0RScrj/banner.jpg" style="margin: 0px auto; height: 100vh; display: flex;"></div>';
+  if (window.location.origin.includes(element.link)) {
+    document.getElementsByTagName("body")[0].innerHTML =
+      '<div><img src="https://i.ibb.co/r0RScrj/banner.jpg" style="margin: 0px auto; height: 100vh; display: flex;"></div>';
   }
 });
